678cd54305a7e952b4b6e5a670778d1d2b8e9405
Samuel Thand <sath2102@student.miun.se>
2023-05-08 17:18:31 +0200

    added thoughts

--- src/Maze.java
+++ src/Maze.java
@@ -7,6 +7,13 @@ public class Maze {
         // TODO ska vi processera bilder från mazestoprint eller hitta/skapa andra bilder som inte behöver preprocessas?
 
 
+        // TODO ide:
+        // Ta bort vita mellanrummet utanför labyrinten genom att räkna antal rader och kolumner tills den slår i svart
+        // Ignorera rött och grönt
+        // Ta resultatlabyrinten med svart border och downscalea så att pathsen bara blir 1 pixel och väggar 1 pixel
+        // Generera maze som matrix utifrån deta
+        // Gör funktionalitet för att markera start och finish cell och låt användaren sätta ut start och finish i GUI
+
     }
 
     private void loadMaze(File mazeImage) {
