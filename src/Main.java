61008ae468161c976e249b82b2ecd56d75e74517
Samuel Thand <sath2102@student.miun.se>
2023-05-08 16:36:00 +0200

    Started reasoning about task

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import java.io.File;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("run");
+
+        var maze = new Maze(new File("src/maze-image.jpg"));
+
+    }
+
+}
---
+++ src/Maze.java
@@ -0,0 +1,20 @@
+import java.io.File;
+
+public class Maze {
+
+    public Maze(File mazeImage) {
+
+        // TODO ska vi processera bilder från mazestoprint eller hitta/skapa andra bilder som inte behöver preprocessas?
+
+
+    }
+
+    private void loadMaze(File mazeImage) {
+
+    }
+
+    private void processImage(File mazeImage) {
+
+    }
+
+}
